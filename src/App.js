1e6bca37d3889ee01fc3c2d0156e44a638b231a1
Anuoluwapo Omobolaji <francismobolaji1990@gmail.com>
2022-09-16 01:28:22 +0100

    basic navigation setup

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 
 const App = () => {
-  const [darkTheme, setDarkTheme] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
-      <Routes/>
-      <Navbar/>
-      <Footer/>
+    <div className={darkTheme ? 'dark' : ''}>
+        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+          <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+          <Routes/>
+          <Footer/>
+        </div>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
-const Navbar = () => {
+import { Link } from 'react-router-dom'
+import Search from './Search'
+const Navbar = (props) => {
+    const {darkTheme, setDarkTheme} = props
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className='p-5 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 shadow-md' >
+    <div className="flex justify-between items-center space-x-5 w-screen">
+        <Link to='/'>
+            <p className="text-2xl bg-blue-500 font-bold py-2 px-4 text-gray-100 dark:text-gray-800 dark:bg-gray-400" >
+                Search.
+            </p>
+        </Link>
+        <button className='py-2 px-4 rounded text-gray-800 bg-gray-200 dark:bg-gray-700
+         shadod-sm hover:shadow-md dark:text-gray-100' onClick={()=>setDarkTheme(!darkTheme)}>{darkTheme ? 'Light 💡' : 'Dark 🌑'}</button>
+    </div>
+    {/* <Search/> */}
     </div>
   )
 }
